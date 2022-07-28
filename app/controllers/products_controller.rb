b44fcc86e192f64e5a47b5a448fb87401bc2b4b8
alijm0806 <alijm1104@gmail.com>
2022-07-28 17:01:52 -0500

    CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,34 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params["id"])
     render json: product.as_json
   end
+
+  def create
+    p params[:name]
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url],
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  def update
+    p params[:name]
+    product = product(
+      product.name = params[:name],
+      product.price = params[:price],
+      product.description = params[:description],
+      product.image_url = params[:image_url]
+    )
+    product.save
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: { message: "product has been removed" }
+  end
 end
