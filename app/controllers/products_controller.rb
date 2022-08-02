78d8affbfeac189f904d5b4c3d9db40afc0dfb89
alijm0806 <alijm1104@gmail.com>
2022-08-02 12:18:56 -0500

    add index/show.json

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,39 +1,39 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+    render template: "products/index"
   end
 
   def show
-    product = Product.find_by(id: params["id"])
-    render json: product.as_json(methods: [:friendly_created_at, :is_discounted?, :tax, :total])
+    @product = Product.find_by(id: params["id"])
+    render template: "products/show"
   end
 
   def create
     p params[:name]
-    product = Product.new(
+    @product = Product.new(
       name: params[:name],
       price: params[:price],
       description: params[:description],
       image_url: params[:image_url],
     )
     product.save
-    render json: product.as_json
+    render template: "products/show"
   end
 
   def update
-    product = Product.find_by(id: params["id"])
-    product.name = params[:name] || product.name
-    product.price = params[:price] || product.price
-    product.description = params[:description] || product.description
-    product.image_url = params[:image_url] || product.image_url
+    @product = Product.find_by(id: params["id"])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.description = params[:description] || @product.description
+    @product.image_url = params[:image_url] || @product.image_url
 
     product.save
-    render json: product.as_json
+    render template: "products/show"
   end
 
   def destroy
-    product = Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
     product.destroy
     render json: { message: "product has been removed" }
   end
